4a0105ef5a0a2f5495c1e745a7de3f9dcf670c71
Rene Kabanda <2495191+kabandr@users.noreply.github.com>
2022-05-17 18:32:15 +0100

    removed unused code.

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,21 +5,9 @@ import {
   getUser,
   updateUser,
 } from "../controllers/user.js";
-import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
+import { verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
-
-// router.get("/checkauthentication", verifyToken, (req, res, next) => {
-//   res.send("Hello user, you are logged in!");
-// });
-
-// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
-//   res.send("Hello user, you are logged in and you can delete your account.");
-// });
-
-// router.get("/checkadmin/:id", verifyUser, (req, res, next) => {
-//   res.send("Hello admin, you are logged in and you can delete all accounts.");
-// });
 
 // UPDATE
 router.put("/:id", verifyUser, updateUser);
